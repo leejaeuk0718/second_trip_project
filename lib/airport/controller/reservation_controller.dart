5d2b6381725b4b56f49946d8429810c11b21ca11
wonderhyenstudy <wonderhyenstudy@gmail.com>
2026-04-22 12:40:46 +0900

    feat : 내예약내역 높이잘림현상, api import 삭제

--- lib/airport/controller/reservation_controller.dart
+++ lib/airport/controller/reservation_controller.dart
@@ -3,7 +3,6 @@ import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import 'package:second_trip_project/util/api_client.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import '../../common/constants/api_constants.dart';
 import '../../services/member_service.dart';
 import '../model/reservation_item.dart';
 
